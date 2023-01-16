39e68918c7f163609b738760a8d2c88e1ecc9c13
szymqn <koodziejskisz@outlook.com>
2023-01-16 23:28:42 +0100

    settings template

---
+++ settings.py
@@ -0,0 +1,19 @@
+import pygame
+import pygame_menu
+
+
+class Settings:
+    def __init__(self, screen, screen_height, screen_width):
+        self.screen = screen
+        self.screen_height = screen_height
+        self.screen_width = screen_width
+        self.status = False
+
+    def set_difficulty(self):
+        pass
+
+    def submit(self):
+        self.status = True
+
+    def draw(self):
+        pass
