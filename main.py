eebc83bf57549375596a7fc45a236970ff57a373
szymqn <koodziejskisz@outlook.com>
2023-02-02 01:27:56 +0100

    update GameState

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 from game import Game
-from start import Start
+from intro import Intro
+from settings import Settings
 from crt import CRT
 
 
@@ -10,22 +11,25 @@ class GameState:
         self.state = 'intro'
 
     def intro(self):
-        game = Game(*screen_setting)
-        game.run()
+        intro = Intro(screen, screen.get_height(), screen.get_width())
+        intro.draw()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == ALIENLASER and start.status is False:
-                game.alien_shoot()
+    def main_game(self):
+        game = Game(screen, screen.get_height(), screen.get_width())
+        game.draw()
 
-        pygame.display.flip()
+    def settings(self):
+        settings = Settings(screen, screen.get_height(), screen.get_width())
+        settings.draw()
 
     def state_manager(self):
         match self.state:
             case 'intro':
                 self.intro()
+            case 'main_game':
+                self.main_game()
+            case 'settings':
+                self.settings()
 
 
 if __name__ == '__main__':
@@ -34,8 +38,7 @@ if __name__ == '__main__':
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
-    screen_setting = [screen, screen_height, screen_height]
-    crt = CRT(*screen_setting)
+    crt = CRT(screen, screen.get_height(), screen.get_width())
     game_state = GameState()
 
     ALIENLASER = pygame.USEREVENT + 1
@@ -46,4 +49,10 @@ if __name__ == '__main__':
         crt.draw()
         game_state.intro()
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
         clock.tick(60)
+        pygame.display.flip()
