f9ea12c544e090013d8739d90d65f22468ea5a6d
szymqn <koodziejskisz@outlook.com>
2023-02-14 01:34:49 +0100

    update draw

--- intro.py
+++ intro.py
@@ -1,6 +1,4 @@
 import pygame
-from leaderboard import Leaderboard
-from settings import Settings
 
 
 class Intro:
@@ -10,40 +8,23 @@ class Intro:
         self.screen_width = screen_width
         self.font = pygame.font.Font('font/Pixeled.ttf', 20)
 
-        screen_settings = [self.screen, self.screen_height, self.screen_width]
-
-        self.leaderboard = Leaderboard(*screen_settings)
-        self.settings = Settings(*screen_settings)
-
     def draw(self):
-        keys = pygame.key.get_pressed()
+        menu_surf = self.font.render('WELCOME TO SPACE INVADERS', False, 'white')
+        menu_rect = menu_surf.get_rect(center=(self.screen_width / 2, (self.screen_height / 2) - 190))
+        self.screen.blit(menu_surf, menu_rect)
 
-        if keys[pygame.K_l]:
-            self.leaderboard.status = True
-        elif keys[pygame.K_s]:
-            self.settings.status = True
+        start_surf = self.font.render('PRESS E TO START', False, 'white')
+        start_rect = menu_surf.get_rect(center=((self.screen_width / 2) + 90, (self.screen_height / 2) - 80))
+        self.screen.blit(start_surf, start_rect)
 
-        if self.leaderboard.status:
-            self.leaderboard.draw()
-        elif self.settings.status:
-            self.settings.draw()
-        else:
-            menu_surf = self.font.render('WELCOME TO SPACE INVADERS', False, 'white')
-            menu_rect = menu_surf.get_rect(center=(self.screen_width / 2, (self.screen_height / 2) - 190))
-            self.screen.blit(menu_surf, menu_rect)
+        lead_surf = self.font.render('PRESS L TO LEADERBOARD', False, '#AA6C39')
+        lead_rect = lead_surf.get_rect(center=((self.screen_width / 2) + 10, (self.screen_height / 2) + 10))
+        self.screen.blit(lead_surf, lead_rect)
 
-            start_surf = self.font.render('PRESS E TO START', False, 'white')
-            start_rect = menu_surf.get_rect(center=((self.screen_width / 2) + 90, (self.screen_height / 2) - 80))
-            self.screen.blit(start_surf, start_rect)
+        settings_surf = self.font.render('PRESS S TO SETTINGS', False, 'white')
+        settings_rect = lead_surf.get_rect(center=((self.screen_width / 2) + 40, (self.screen_height / 2) + 100))
+        self.screen.blit(settings_surf, settings_rect)
 
-            lead_surf = self.font.render('PRESS L TO LEADERBOARD', False, '#AA6C39')
-            lead_rect = lead_surf.get_rect(center=((self.screen_width / 2) + 10, (self.screen_height / 2) + 10))
-            self.screen.blit(lead_surf, lead_rect)
-
-            settings_surf = self.font.render('PRESS S TO SETTINGS', False, 'white')
-            settings_rect = lead_surf.get_rect(center=((self.screen_width / 2) + 40, (self.screen_height / 2) + 100))
-            self.screen.blit(settings_surf, settings_rect)
-
-            quit_surf = self.font.render('PRESS Q TO QUIT', False, 'white')
-            quit_rect = quit_surf.get_rect(center=((self.screen_width / 2) + 0, (self.screen_height / 2) + 190))
-            self.screen.blit(quit_surf, quit_rect)
+        quit_surf = self.font.render('PRESS Q TO QUIT', False, 'white')
+        quit_rect = quit_surf.get_rect(center=((self.screen_width / 2) + 0, (self.screen_height / 2) + 190))
+        self.screen.blit(quit_surf, quit_rect)
