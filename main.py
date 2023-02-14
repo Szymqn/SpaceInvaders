89bba5a1467d41263035d83bc9750d4f2a69c547
szymqn <koodziejskisz@outlook.com>
2023-02-14 01:34:06 +0100

    update GameState

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
 from game import Game
-from intro import IntroState
+from intro import Intro
+from leaderboard import Leaderboard
+from settings import Settings
 from crt import CRT
 
 
@@ -11,12 +13,29 @@ class GameState:
         self.screen_settings = [screen, screen_height, screen_width]
         self.state_manager()
 
-    def intro(self):
-        IntroState(*self.screen_settings)
-
     def main_game(self):
         game = Game(*self.screen_settings)
         game.draw()
+
+    def intro(self):
+        self.state = 'intro'
+        intro = Intro(*self.screen_settings)
+        intro.draw()
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_l]:
+            self.state = 'leaderboard'
+        elif keys[pygame.K_s]:
+            self.state = 'settings'
+
+    def leaderboard(self):
+        leaderboard = Leaderboard(*self.screen_settings)
+        leaderboard.draw()
+
+    def settings(self):
+        settings = Settings(*self.screen_settings)
+        settings.draw()
 
     def state_manager(self):
         match self.state:
@@ -24,6 +43,10 @@ class GameState:
                 self.intro()
             case 'main_game':
                 self.main_game()
+            case 'leaderboard':
+                self.leaderboard()
+            case 'settings':
+                self.settings()
 
 
 if __name__ == '__main__':
@@ -33,15 +56,15 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
     crt = CRT(screen, screen.get_height(), screen.get_width())
-    game_state = GameState()
 
     ALIENLASER = pygame.USEREVENT + 1
     pygame.time.set_timer(ALIENLASER, 800)
+    game_state = GameState()
 
     while True:
         screen.fill((30, 30, 30))
         crt.draw()
-        game_state.intro()
+        game_state.state_manager()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
