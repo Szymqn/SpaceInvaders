11ce8b8513aab0d8615393cfd09f8168164dd648
szymqn <koodziejskisz@outlook.com>
2023-02-19 00:09:38 +0100

    disable test

--- test_game.py
+++ test_game.py
@@ -3,7 +3,6 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_get_level(self):
-
         # self.level = settings.get_level()
         # self.assertEqual(settings.get_level(), 1)
         pass
